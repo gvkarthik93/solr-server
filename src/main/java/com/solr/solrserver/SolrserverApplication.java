0c942cf6520d32a7ba31f5305fd6b43a5f75561c
Karthik <gvkarthik93@gmail.com>
2018-06-05 18:02:21 -0400

    Initial Commit

---
+++ src/main/java/com/solr/solrserver/SolrserverApplication.java
@@ -0,0 +1,34 @@
+package com.solr.solrserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@SpringBootApplication
+public class SolrserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolrserverApplication.class, args);
+	}
+	
+	@RestController
+	class SolrServer {
+	    
+	    @RequestMapping("/")
+	    String startserver() {
+	        return "Solr Server Started";
+	    }
+	    
+	    @RequestMapping("/results")
+	    String fetchresults() {
+	        return "Fetching all results from Solr Index";
+	    }
+	}
+}
---
+++ src/test/java/com/solr/solrserver/SolrserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.solr.solrserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=SolrserverApplication.class)
+public class SolrserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
